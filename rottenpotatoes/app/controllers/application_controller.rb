b8683cb64aa3931cdd7b715c12ba13d5ca222c6b
Yifan Jiang <yifanjiang97@gmail.com>
2020-03-11 00:27:22 -0500

    f48b81fba346aae

--- rottenpotatoes/app/controllers/application_controller.rb
+++ rottenpotatoes/app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 end
+
+if RUBY_VERSION>='2.6.0'
+  if Rails.version < '5'
+    class ActionController::TestResponse < ActionDispatch::TestResponse
+      def recycle!
+        # hack to avoid MonitorMixin double-initialize error:
+        @mon_mutex_owner_object_id = nil
+        @mon_mutex = nil
+        initialize
+      end
+    end
+  else
+    puts "Monkeypatch for ActionController::TestResponse no longer needed"
+  end
+end
